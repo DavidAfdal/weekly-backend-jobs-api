38fc48ebbcfc8c3fda49acf3813b6945414f44ce
David Afdal Kaizar Mutahadi <davidafdal7@gmail.com>
2023-11-20 21:28:22 +0700

    commit

--- models/applyier_models.go
+++ models/applyier_models.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Apllier struct {
-	Id   string  `gorm:"type:int;primaryKey" json:"id"`
+	Id   int  `gorm:"type:int;primaryKey" json:"id"`
 	UserID string `json:"userId"`
 	Jobs []Job  `gorm:"many2many:applier_job;constraint:OnDelete:CASCADE;" json:"jobs"`
 	CreatedAt time.Time `json:"createdAt"`
